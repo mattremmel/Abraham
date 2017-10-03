489a4521db209b70749e46c3310cbe6169ce2754
Matthew Remmel <matt.remmel@gmail.com>
2017-10-03 11:01:41 -0400

    Added unsigned byte type

---
+++ include/abraham/utilities/Types.hpp
@@ -0,0 +1,19 @@
+//
+// Created by Matthew Remmel on 10/1/17.
+//
+
+#ifndef ABRAHAM_TYPES_HPP
+#define ABRAHAM_TYPES_HPP
+
+#include <cstdint>
+
+
+namespace abraham {
+
+    /**
+     * Typedef for working with unsigned bytes.
+     */
+    typedef uint8_t ubyte;
+}
+
+#endif //ABRAHAM_TYPES_HPP
---
+++ src/utilities/Types.cpp
@@ -0,0 +1,9 @@
+//
+// Created by Matthew Remmel on 10/1/17.
+//
+
+#include "Types.hpp"
+
+/**
+ * Types implementation is header only.
+ */
